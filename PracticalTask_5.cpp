3e2e46b7754917921b881f908afa2452ef19ce5c
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2023-03-26 14:52:46 +0500

    Ready v0.1

--- PracticalTask_5.cpp
+++ PracticalTask_5.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	int number;
+	cout << "Enter your number: ";
+	cin >> number;
+	int result = 1;
+
+	__asm
+	{
+		push number
+		call fact
+		mul[esp]
+		add esp, 4
+		mov result, eax
+		jmp exit1
+		fact :
+			cmp[esp + 4], 1
+			jbe returnOne
+			mov eax, 1
+			mov esi, [esp + 4]
+			dec esi
+			push esi
+			call fact
+			mul[esp]
+			add esp, 4
+			ret
+		returnOne :
+			mov eax, 1
+			ret
+		exit1 :
+	}
+
+	cout << "Factorial = " << result;
 }
